3f8e4a470a6b34f01c541179348752b1046f5939
Mikhail Fryshkin <muxazila@mail.ru>
2022-08-27 23:41:38 +0700

    123

--- Инвентаризация.py
+++ Инвентаризация.py
@@ -53,21 +53,22 @@ def comparison():
         with open('base.csv', newline='', encoding='utf-8-sig') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                if row['Код \nноменклатуры'] not in art_list:
-                    art_list.append(row['Код \nноменклатуры'])
-                    result_list.append([row['Местоположение'],
-                                        row['Код \nноменклатуры'],
-                                        row['Описание товара'],
-                                        int(row['Физические \nзапасы'].replace('.0', '')
-                                            if row['Физические \nзапасы'] != ''
-                                            else row['Физические \nзапасы'].replace('', '0')),
-                                        int(row['Зарезерви\nровано'].replace('.0', '')
-                                            if row['Зарезерви\nровано'] != ''
-                                            else row['Зарезерви\nровано'].replace('', '0')),
-                                        int(row['Доступно'].replace('.0', '')
-                                            if row['Доступно'] != ''
-                                            else row['Доступно'].replace('', '0')),
-                                        0])
+                if row['Номер документа'] == '':
+                    if row['Код \nноменклатуры'] not in art_list:
+                        art_list.append(row['Код \nноменклатуры'])
+                        result_list.append([row['Местоположение'],
+                                            row['Код \nноменклатуры'],
+                                            row['Описание товара'],
+                                            int(row['Физические \nзапасы'].replace('.0', '')
+                                                if row['Физические \nзапасы'] != ''
+                                                else row['Физические \nзапасы'].replace('', '0')),
+                                            int(row['Зарезерви\nровано'].replace('.0', '')
+                                                if row['Зарезерви\nровано'] != ''
+                                                else row['Зарезерви\nровано'].replace('', '0')),
+                                            int(row['Доступно'].replace('.0', '')
+                                                if row['Доступно'] != ''
+                                                else row['Доступно'].replace('', '0')),
+                                            0])
     except Exception as ex:
         logger.debug(ex)
         time.sleep(30)
@@ -84,8 +85,6 @@ def comparison():
                 if row['Код номенклатуры'] not in check_art:
                     check_art.append(row['Код номенклатуры'])
 
-        print('Сверка, ожидайте...')
-
         print('Расхождения:')
 
         for item in check_cells:
@@ -97,14 +96,14 @@ def comparison():
                             if row['Код номенклатуры'] in art_list and row['Код номенклатуры'] == i[1] and i[0] == item:
                                 i[-1] = int(row['Количество факт'])
                                 result.append(i)
-                                logger.info(i)
+                                print('Найденно расхождение: {}'.format([i[0], i[1], i[2]]))
                             elif row['Код номенклатуры'] not in art_list \
                                     and row['Код номенклатуры'] not in [q[1] for q in result]:
                                 result.append([item, row['Код номенклатуры'], '', 0, 0, 0, int(row['Количество факт'])])
-                                logger.info([item, row['Код номенклатуры'], '', 0, 0, 0, int(row['Количество факт'])])
+                                print('Найденно расхождение: {}'.format([item, row['Код номенклатуры']]))
                             elif i[0] == item and i[1] not in check_art and i[1] not in [w[1] for w in result]:
                                 result.append(i)
-                                logger.info(i)
+                                print('Найденно расхождение: {}'.format([i[0], i[1], i[2]]))
 
         for i in sorted(result):
             delta = i[-1] - i[3]
@@ -126,17 +125,31 @@ def comparison():
         time.sleep(120)
 
 
-def align_left(x):
-    return ['text-align: left' for x in x]
-
-
 def write_exsel(name):
     try:
+        writer = pd.ExcelWriter('{}.xlsx'.format(name), engine='xlsxwriter')
         df = pd.read_csv('{}.csv'.format(name), encoding='utf-8-sig', delimiter=";")
-        writer = pd.ExcelWriter('{}.xlsx'.format(name))
-        df.style.apply(align_left, axis=0).to_excel(writer, sheet_name='Sheet1', index=False, na_rep='NaN')
-        writer.sheets['Sheet1'].set_column(0, 8, 20)
-        writer.save()
+        df.to_excel(writer, sheet_name='Sheet1', index=False, na_rep='NaN')
+        workbook = writer.book
+        worksheet = writer.sheets['Sheet1']
+
+        cell_format = workbook.add_format()
+        cell_format.set_bold()
+        cell_format.set_num_format('[Green]General;[Red]-General;General')
+
+        cell_format2 = workbook.add_format()
+        cell_format2.set_align('left')
+
+        cell_format3 = workbook.add_format()
+        cell_format3.set_align('center')
+
+        worksheet.set_column('A:B', 18, cell_format2)
+        worksheet.set_column('C:C', 80, cell_format2)
+        worksheet.set_column('D:H', 12, cell_format3)
+        worksheet.set_column('H:H', 12, cell_format)
+
+        writer.close()
+
     except Exception as ex:
         logger.debug(ex)
     finally:
@@ -164,6 +177,11 @@ def write_result(result: list, name: str):
         time.sleep(30)
 
 
+def color_negative_red(val):
+    color = 'red' if val < 0 else 'black'
+    return 'color: %s' % color
+
+
 def main():
     read_file(file_name())
     comparison()
@@ -171,5 +189,4 @@ def main():
 
 if __name__ == "__main__":
     logger.add(sys.stderr, format="{time} {level} {message}", filter="my_module", level="INFO")
-    logger.add("logs/file_X.log", retention="7 days")
     main()
